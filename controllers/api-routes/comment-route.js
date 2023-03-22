9380043b48aabf539dc72434bc2c8b8397079d21
Eli Baynes <sbaynes1994@gmail.com>
2023-03-21 19:29:22 -0500

    routes to POST, PUT, DELETE comments

---
+++ controllers/api-routes/comment-route.js
@@ -0,0 +1,52 @@
+const router = require('express').Router();
+const { Comment } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+// route to create a new comment
+router.post('/', withAuth, async (req, res) => {
+    const body = req.body;
+    try {
+        const commentData = await Comment.create({ ...body, user_id: req.session.user_id });
+        res.status(200).json(commentData);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+// route to update a comment
+router.put('/:id', withAuth, async (req, res) => {
+    try {
+        const [affectedRows] = await Comment.update(req.body, {
+            where: {
+                id: req.params.id,
+            },
+        });
+        if (affectedRows > 0) {
+            res.status(200).end();
+        } else {
+            res.status(404).end();
+        }
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// route to delete a comment
+router.delete('/:id', withAuth, async (req, res) => {
+    try {
+        const [affectedRows] = await Comment.destroy({
+            where: {
+                id: req.params.id,
+            },
+        });
+        if (affectedRows > 0) {
+            res.status(200).end();
+        } else {
+            res.status(404).end();
+        }
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router;
