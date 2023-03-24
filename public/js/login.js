da57d984cc9c75aac109c8629f2f53e252ed191e
Eli Baynes <sbaynes1994@gmail.com>
2023-03-23 23:07:44 -0500

    login and signup forms

---
+++ public/js/login.js
@@ -0,0 +1,27 @@
+async function submitFormHandler(event) {
+    event.preventDefault();
+
+    const username = document.getElementById('username').value.trim();
+    const password = document.getElementById('password').value.trim();
+
+    if (username && password) {
+        const response = await fetch('/api/users/login', {
+            method: 'post',
+            body: JSON.stringify({
+                username,
+                password
+            }),
+            headers: { 'Content-Type': 'application/json' }
+        });
+
+        if (response.ok) {
+            document.location.replace('/profile');
+        } else {
+            alert(response.statusText);
+        }
+    } else {
+        alert('Please fill out all fields');
+    }
+};
+
+document.querySelector('.login-form').addEventListener('submit', submitFormHandler);
---
+++ public/js/signup.js
@@ -0,0 +1,27 @@
+async function signupFormHandler(event) {
+    event.preventDefault();
+
+    const username = document.getElementById('username').value.trim();
+    const password = document.getElementById('password').value.trim();
+
+    if (username && password) {
+        const response = await fetch('/api/users', {
+            method: 'post',
+            body: JSON.stringify({
+                username,
+                password
+            }),
+            headers: { 'Content-Type': 'application/json' }
+        });
+
+        if (response.ok) {
+            document.location.replace('/profile');
+        } else {
+            alert(response.statusText);
+        }
+    } else {
+        alert('Please fill out all fields');
+    }
+}
+
+document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
