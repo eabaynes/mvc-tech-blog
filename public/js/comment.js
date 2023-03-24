3abfbcc69379337eca8c543f179e954f0d64efa0
Eli Baynes <sbaynes1994@gmail.com>
2023-03-23 23:40:43 -0500

    comment submit handler

---
+++ public/js/comment.js
@@ -0,0 +1,26 @@
+async function commentSubmitHandler(event) {
+    event.preventDefault();
+
+    const post_id = document.getElementById('post-id').value;
+    const comment_text = document.getElementById('comment-text').value.trim();
+    const user_id = req.session.user_id;
+
+    if (comment_text) {
+        const response = await fetch(`/api/posts/${post_id}/comments`, {
+            method: 'POST',
+            body: JSON.stringify({
+                comment: comment_text,
+                user_id: user_id
+                }),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+
+            if (response.ok) {
+                document.location.reload();
+            } else {
+                alert(response.statusText);
+            }
+    }
+}
