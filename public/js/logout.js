fa5d8030e0ce3d5555947f09ad280653ce9fa6ec
Eli Baynes <sbaynes1994@gmail.com>
2023-03-23 23:45:13 -0500

    add logout handler

---
+++ public/js/logout.js
@@ -0,0 +1,15 @@
+async function logoutHandler(event) {
+    event.preventDefault();
+    const response = await fetch('/api/users/logout', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' }
+    });
+
+    if (response.ok) {
+        document.location.replace('/');
+    } else {
+        alert(response.statusText);
+    }
+}
+
+document.getElementById('logout').addEventListener('click', logoutHandler);
