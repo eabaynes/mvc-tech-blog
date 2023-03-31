7ca258ae6adcb875449d0c61a7d601895a21cf56
Eli Baynes <sbaynes1994@gmail.com>
2023-03-31 00:14:19 -0500

    get server working

--- models/index.js
+++ models/index.js
@@ -1,21 +1,26 @@
 const Comment = require("./Comment.js");
-const { belongsTo, hasMany } = require("./Post.js");
 const User = require("./User.js");
 const Post = require("./Post.js");
 
+// create associations
+User.hasMany(Post, {
+    foreignKey: "user_id",
+    allowNull: true,
+});
+
 Post.belongsTo(User, {
     foreignKey: "user_id",
-    onDelete: "CASCADE",
-    });
-
-Post.hasMany(Comment, {
-    foreignKey: "post_id",
-    onDelete: "CASCADE",
-    });
+    allowNull: true,
+});
 
 Comment.belongsTo(User, {
     foreignKey: "user_id",
-    onDelete: "CASCADE",
-    });
+    allowNull: true,
+});
+
+Comment.belongsTo(Post, {
+    foreignKey: "post_id",
+    allowNull: true,
+});
 
 module.exports = { Comment, User, Post };
