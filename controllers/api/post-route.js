fcdb9bec1198c3142110439ad8f2723312c1d2ce
Eli Baynes <sbaynes1994@gmail.com>
2023-04-04 00:53:17 -0500

    able to add comments

--- controllers/api/post-route.js
+++ controllers/api/post-route.js
@@ -32,22 +32,4 @@ router.put('/:id', withAuth, async (req, res) => {
     }
 });
 
-// route to delete a post
-router.delete('/:id', withAuth, async (req, res) => {
-    try {
-        const [affectedRows] = await Post.destroy({
-            where: {
-                id: req.params.id,
-            },
-        });
-        if (affectedRows > 0) {
-            res.status(200).end();
-        } else {
-            res.status(404).end();
-        }
-    } catch (err) {
-        res.status(500).json(err);
-    }
-});
-
 module.exports = router;
--- public/js/comment.js
+++ public/js/comment.js
@@ -1,26 +1,28 @@
 async function commentSubmitHandler(event) {
     event.preventDefault();
 
-    const post_id = document.getElementById('post-id').value;
     const comment_text = document.getElementById('comment-text').value.trim();
+    console.log(comment_text);
+    const post_id = document.getElementById('post-id').innerHTML
+    console.log(post_id);
 
     if (comment_text) {
-        const response = await fetch(`/api/comments`, {
+        const response = await fetch('/api/comments', {
             method: 'POST',
             body: JSON.stringify({
-                post_id: post_id,
-                comment: comment_text,
-                }),
-                headers: {
-                    'Content-Type': 'application/json'
-                }
-            });
-
-            if (response.ok) {
-                document.location.reload();
-            } else {
-                alert(response.statusText);
+                body: comment_text,
+                post_id: post_id
+            }),
+            headers: {
+                'Content-Type': 'application/json'
             }
+        });
+
+        if (response.ok) {
+            document.location.reload();
+        } else {
+            alert(response.statusText);
+        }
     }
 }
 
